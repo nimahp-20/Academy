c072dbe9dbabbfb7d6064bfcb7e2573abad84837
unknown <nimaneon17@gmail.com>
2024-09-22 18:48:42 +0330

    user password hashed and jwt accessToken added

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,5 +1,8 @@
 const userModel = require('../../models/user')
 const registerValidator = require('./../../validators/register')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 exports.register = async (req, res) => {
     const validationResult = registerValidator(req.body)
@@ -17,6 +20,25 @@ exports.register = async (req, res) => {
             message: 'username or email is duplicated'
         })
     }
+
+    const countOfUsers = await userModel.count()
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        email,
+        username,
+        name,
+        password: hashedPassword,
+        phone,
+        role: countOfUsers > 0 ? "USER" : "ADMIN"
+    })
+
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        exoireIn: "30 day"
+    })
+
+    return res.status(201).json({ user, accessToken })
 }
 
 exports.login = async (req, res) => {
