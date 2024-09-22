3b9b4b717f3ec58047476db97d40df71e165daae
unknown <nimaneon17@gmail.com>
2024-09-22 18:33:01 +0330

    validator added to auth controller

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express')
 const authRouter = require('./routes/v1/auth')
 const cors = require('cors')
 const path = require('path')
+const bodyParser = require('body-parser')
+
+
 
 const app = express()
-app.use('/courses/covers')
+app.use('/courses/covers', express.static(path.join(__dirname, "public", "courses", "covers")))
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.use('/v1/auth', authRouter)
 
 module.exports = app
--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,8 +1,22 @@
-const userMode = require('../../models/user')
-
+const userModel = require('../../models/user')
+const registerValidator = require('./../../validators/register')
 
 exports.register = async (req, res) => {
+    const validationResult = registerValidator(req.body)
+    if (validationResult !== true) {
+        return res.status(422).json(validationResult)
+    }
+    const { username, name, email, password, phone } = req.body
 
+    const isUserExists = await userModel.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (isUserExists) {
+        return res.status(409).json({
+            message: 'username or email is duplicated'
+        })
+    }
 }
 
 exports.login = async (req, res) => {
